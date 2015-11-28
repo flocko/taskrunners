439908091f4a7ba08d52df54bf02696364696ec6
Florian Kircher <flo.kircher@gmail.com>
2015-11-28 20:18:51 +0100

    gulpfile initial vars and paths

---
+++ gulp-wpdev-postcss/gulpfile.js
@@ -0,0 +1,66 @@
+/**
+ * ---------------------------
+ * Available Tasks
+ * ---------------------------
+ * gulp      - development tasks for css/js/php files
+ * gulp exec - install bower dependencies and copy files
+ */
+
+/**
+ * ---------------------------
+ * Plugins
+ * ---------------------------
+ * gulp
+ * bower
+ * browser-sync
+ * gulp-plumber
+ * gulp-bower
+ * run-sequence
+ * gulp-load-plugins
+ */
+
+var gulp           = require('gulp');
+var plugins        = require('gulp-load-plugins');
+var browserSync    = require('browser-sync');
+var reload         = browserSync.reload;
+var runSequence    = require('run-sequence');
+var mainBowerFiles = require('main-bower-files');
+
+/**
+ * ---------------------------
+ * Paths and Options
+ * ---------------------------
+ */
+
+var theme = {
+  name : 'slouchy',
+  url  : 'wp-content/themes/slouchy'
+}
+
+var options = {
+  pcss  : {
+    files : 'pcss/**/*.pcss',
+    dest  : 'pcss/'
+  },
+  img   : {
+    files : 'img/**/*.*',
+    dest  : 'img/'
+  },
+  php   : {
+    files : '**/*.php'
+  },
+  js    : {
+    files : 'js/**/*.js',
+    dest  : 'js/'
+  },
+  fonts : {
+    files: 'fonts/**/*',
+    dest : 'fonts/'
+  }
+}
+
+var vendor = {
+  mdi : {
+    css : 'css/vendor/mdi'
+  }
+}
