cce839576622678b048fc4deeaf94d6ca100f2c4
Florian Kircher <flo.kircher@gmail.com>
2015-11-29 21:13:33 +0100

    Update gulpfile.js

--- gulp-wpdev-scss/gulpfile.js
+++ gulp-wpdev-scss/gulpfile.js
@@ -20,7 +20,7 @@
  */
 
 var gulp           = require('gulp');
-var plugins        = require('gulp-load-plugins');
+var plugins        = require('gulp-load-plugins')();
 var browserSync    = require('browser-sync');
 var reload         = browserSync.reload;
 var runSequence    = require('run-sequence');
@@ -34,13 +34,17 @@ var mainBowerFiles = require('main-bower-files');
 
 var theme = {
   name : 'slouchy',
-  url  : 'wp-content/themes/slouchy'
+  url  : 'wp-content/themes/slouchy/'
 }
 
 var options = {
   sass  : {
-    files : 'scss/**/*.scss',
-    dest  : 'scss/'
+    files : 'sass/**/*.scss',
+    dest  : 'sass/'
+  },
+  css   : {
+    file  : 'style.css',
+    dest : 'css/'
   },
   img   : {
     files : 'img/**/*.*',
@@ -51,6 +55,7 @@ var options = {
   },
   js    : {
     files : 'js/**/*.js',
+    file  : 'app.js',
     dest  : 'js/'
   },
   fonts : {
@@ -64,3 +69,90 @@ var vendor = {
     sass : 'sass/vendor/mdi'
   }
 }
+
+/**
+ * ---------------------------
+ * Main Tasks
+ * ---------------------------
+ */
+
+gulp.task('default', ['dev']);
+
+gulp.task('exec', function(done) {
+  runSequence('bower:install', 'bower:copy', done);
+});
+
+
+/**
+ * ---------------------------
+ * Development Tasks
+ * ---------------------------
+ */
+
+// start dev server and watch for file changes
+gulp.task('dev', ['sass', 'minify:js'], function() {
+  browserSync({
+    proxy: "http://localhost/wpbeta"
+  });
+
+  gulp.watch(theme.url + options.sass.files, ['sass']);
+  gulp.watch(theme.url + options.php.files).on('change', reload);
+  gulp.watch(theme.url + options.js.files, ['minify:js']);
+  //gulp.watch(theme.url + options.js.files).on('change', reload);
+
+});
+
+// compile sass
+gulp.task('sass', function() {
+  gulp.src( theme.url + options.sass.files )
+  .pipe( plugins.plumber() )
+  .pipe( plugins.sass( { outputStyle : 'compressed' }).on('error', plugins.sass.logError))
+  .pipe( plugins.autoprefixer( {
+    browsers: ['last 2 versions'],
+      cascade: false
+    }))
+  .pipe(gulp.dest( theme.url ))
+  .pipe(reload({stream: true}));
+});
+
+// concat all js files for build
+gulp.task('minify:js', function() {
+  return gulp.src(theme.url + options.js.files)
+  .pipe( plugins.plumber() )
+  .pipe( plugins.concat( options.js.file ) )
+  .pipe( plugins.uglify() )
+  .pipe( gulp.dest( theme.url ) )
+  .pipe(reload({stream:true}));
+
+});
+
+/**
+ * ---------------------------
+ * Exec Tasks
+ * ---------------------------
+ **/
+
+// install bower components
+gulp.task('bower:install', function() {
+  return plugins.bower({ cmd: 'install'});
+});
+
+// copy bower files
+gulp.task('bower:copy', function() {
+  var jsFilter   = plugins.filter('*.js', {restore:true});
+  var scssFilter = plugins.filter('*.scss', {restore:true});
+  var fontFilter = plugins.filter(['*.eot', '*.woff', '*.svg', '*.ttf', '*.woff2'], {restore:true});
+
+  return gulp.src( mainBowerFiles({includeDev:true}) )
+  .pipe( plugins.plumber() )
+  .pipe( fontFilter )
+  .pipe( gulp.dest( theme.url + options.fonts.dest ) )
+  .pipe( fontFilter.restore )
+
+  .pipe( scssFilter )
+  .pipe( gulp.dest (theme.url + vendor.mdi.sass ))
+  .pipe( scssFilter.restore )
+
+  .pipe( jsFilter )
+  .pipe( gulp.dest (theme.url + options.js.dest ));
+});
